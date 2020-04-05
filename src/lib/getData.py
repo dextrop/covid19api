50a788250b17d9f700bccaeab495fd1eeb906699
Saurabh <saurabh@trillbit.com>
2020-04-05 16:14:47 +0530

    Added Load Balancing

--- src/lib/getData.py
+++ src/lib/getData.py
@@ -10,17 +10,22 @@ class getData():
 
     def check_last_scrap(self):
         data_obj = Scrapping.objects.filter()
+        print "Success 1"
         if data_obj.count() < 1:
             data_set = ScrapFromWiki().getDataIndia()
             data = {
                 "data" : json.dumps(data_set)
             }
             Scrapping.objects.create(**data)
+            print "Success 2"
             return False, data_set
         else:
+            print "Success 2.1"
             if ((timezone.now() - data_obj[0].updated).total_seconds() < 1800):
+                print "Success 2.2.0"
                 return False, json.loads(data_obj[0].data)
             else:
+                print "Success 2.2.1"
                 data_set = ScrapFromWiki().getDataIndia()
                 current = data_obj[0]
                 print "Data object to be updated", current
